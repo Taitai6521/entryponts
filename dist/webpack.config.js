66a1cf54f586c94642c3db436bd1ba6b3932b5fc
Taitai6521 <73144809+Taitai6521@users.noreply.github.com>
2020-11-01 10:51:05 +0900

    initial commit

---
+++ dist/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = rewuire('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/javascripts/index.js',
+    output: {
+        path: path.resolve(__dirname,'./dist'),
+        filename: `javascripts/main.js`,
+
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: [
+                    {
+
+                    loader: MiniCssExtractPlugin.loader,
+
+                    }
+                    {
+
+                    loader: 'css-loader',
+                },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename:'./stylesheets/my.css'
+        }),
+        new HtmlWebpackPlugin ({
+            template: './src/index.html',
+        }),
+        new CleanWebpackPlugin(),
+    ],
+
+}
+
+
